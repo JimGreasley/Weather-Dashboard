645b4de752d106a68d4399adc97bb69cfa39c021
Jim Greasley <azjimgreasley@gmail.com>
2020-02-18 21:44:19 -0700

    fixed forecast formatting issues

--- js/javascript.js
+++ js/javascript.js
@@ -1,36 +1,36 @@
 
 $(document).ready(function () {
-    
+
     // Global variables
     // Capture selector for HTML area to hold current weather data  
     var $currentWeather = $("#current-weather");
-    var $cityHistory    = $("#city-history");
+    var $cityHistory = $("#city-history");
 
     // This is my API key.
     var APIKey = "7514abfe02ab6db7877685958ec119d7";
 
     // get Weather Cities history data from local storage
 
-    var data        = localStorage.getItem("WeatherCities");
+    var data = localStorage.getItem("WeatherCities");
 
     if (!data) {
         // create new 'empty' Work Day Schedule array
-        // var weather_cities = [];
-        var weather_cities = ["Phoenix", "Los Angeles", "Seattle"];
+        var weather_cities = [];
+        //var weather_cities = ["Phoenix", "Los Angeles", "Seattle"];
     } else {
         var weather_cities = JSON.parse(data);
     }
 
     // add each city in weather_cities array to history list group
     weather_cities.forEach(loadCity);
-  
 
-   // set Event to save text when corresponding save button is clicked
-   $("#city-list").click(selectCity);
 
-   
+    // set Event to save text when corresponding save button is clicked
+    $("#city-history").click(selectCity);
+
+
     // save button calls
-    $("#searchBtn").on("click", function(event) {
+    $("#searchBtn").on("click", function (event) {
         event.preventDefault();
         // validate search params
         if ($("#search-city").val() === "") {
@@ -43,34 +43,59 @@ $(document).ready(function () {
                 .trim();
             //console.log("city: ", searchCity);
             getCurrentWeather(searchCity);
+            saveSearchCity(searchCity);
         }
-        
+
     });
 
+    function saveSearchCity(srchCity) {
 
-  
-    function loadCity (cityName) {
+        // search weather_cities array for the current search city
+        // if not found then add it to the array
+        // save weather_cities array in local storage
+
+        var add_city_to_history = true;
+        for (let i = 0; i < weather_cities.length; i++) {
+            if (srchCity === weather_cities[i]) {
+                add_city_to_history = false;
+            }
+        }
+
+        if (add_city_to_history) {
+            console.log("adding search city " + srchCity + " to history");
+            weather_cities.push(srchCity);
+        }
 
 
-    var newListItem = $("<button>").addClass("list-group-item list-group-item-action");
-    newListItem.attr("type", "button");
-    newListItem.attr("id", "city-list");
-    newListItem.text(cityName);
+        // save updated text in work_day_schedule array
+        //work_day_schedule[parseInt(idx)] = $(this).siblings(".textarea").val();
 
-    // Append the new timeslot row to the page (Container element)
-    $cityHistory.append(newListItem);
-}
+        // Save updated work_day_schedule array in local storage
+        localStorage.setItem("WeatherCities", JSON.stringify(weather_cities));
 
- 
+    }
+
+    function loadCity(cityName) {
+
+        var newListItem = $("<button>").addClass("list-group-item list-group-item-action");
+        newListItem.attr("type", "button");
+        //newListItem.attr("id", "city-list");
+        newListItem.text(cityName);
+
+        // Append the city from history list to page (Container element)
+        $cityHistory.append(newListItem);
+    }
+
+
     //-----------------------------------------------------------------------------------------
     //  function to select city from history list when its list entry is clicked
     //-----------------------------------------------------------------------------------------
 
     function selectCity(e) {
-        event.preventDefault();  
+        event.preventDefault();
 
         //console.log($(this).siblings(".textarea").val());
-        //console.log($(this).attr("index"));
+        //console.log($(this).attr("data-index"));
         console.log($(this).text());
 
         // capture index (typeof 'string') into work-day-schedule array 
@@ -82,46 +107,49 @@ $(document).ready(function () {
 
         // Save updated work_day_schedule array in local storage
         //localStorage.setItem("WorkDaySchedule", JSON.stringify(work_day_schedule));
-            
+
     }
 
 
 
 
     function getCurrentWeather(city) {
-    
-    // set up the AJAX query URL
+
+        // clear current weather area before displaying any new data
+        $currentWeather.empty();
+
+        // set up the AJAX query URL
 
         console.log("Search for city: ", city);
         //console.log(moment().format("l"));
         //return;
-        
+
         var queryURL =
             "https://api.openweathermap.org/data/2.5/weather?q=" +
             city +
             "&units=imperial&appid="
             + APIKey;
 
-    // AJAX call
+        // AJAX call
 
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function(response) {
+        }).then(function (response) {
             console.log(response);
-            //var now = moment().format("MMMM Do, YYYY");
+            //return;
 
-            var latitude  = response.coord.lat;
+            var latitude = response.coord.lat;
             var longitude = response.coord.lon;
-            var cityID    = response.id;
+            var cityID = response.id;
 
-            var cityRow   = $("<div>").addClass("row");
+            var cityRow = $("<div>").addClass("row");
 
-            var colCityDate = response.name +' (' + moment().format("l") + ')';
+            var colCityDate = response.name + ' (' + moment().format("l") + ')';
 
             var colCity = $("<div>").addClass("col-4 citydate");
             colCity.text(colCityDate);
-            
+
             var colIcon = $("<img>").addClass("col-1");
             colIcon.attr(
                 "src",
@@ -132,7 +160,7 @@ $(document).ready(function () {
 
             $currentWeather.append(cityRow);
 
-            var tempRow   = $("<div>").addClass("row");
+            var tempRow = $("<div>").addClass("row");
             tempRow.text("Temperature: " + response.main.temp + " \u00B0F");
             $currentWeather.append(tempRow);
 
@@ -141,7 +169,7 @@ $(document).ready(function () {
             $currentWeather.append(humidityRow);
 
             var windSpeedRow = $("<div>").addClass("row");
-            windSpeedRow.text("Wind Speed: " + response.wind.speed  + "MPH");
+            windSpeedRow.text("Wind Speed: " + response.wind.speed + "MPH");
             $currentWeather.append(windSpeedRow);
 
             getUVIndex(cityID, latitude, longitude);
@@ -157,14 +185,14 @@ $(document).ready(function () {
     //-------------------------------------------------------------------------------------
 
     function getForecast(cityID) {
-    
+
         // set up the AJAX query URL
-    
+
         console.log("Geting forecast for city ID: " + cityID);
         //console.log(moment().format("l"));
         //return;
-        
-        var queryURL =     
+
+        var queryURL =
             "http://api.openweathermap.org/data/2.5/forecast?id=" + cityID +
             "&units=imperial&appid=" +
             APIKey;
@@ -172,85 +200,91 @@ $(document).ready(function () {
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function(response) {
+        }).then(function (response) {
             console.log(response);
-            
-            var forecastRow   = $("<div>").addClass("row");
+
+            var forecastRow = $("<div>").addClass("row h4");
             forecastRow.text("5-Day Forecast:");
 
-            // loop will start here
-
-            var colForecastDay1 = $("<div>").addClass("col-2");
-            var rowForecastDate1 = $("<p>").addClass("row");
-
-            var js_date = new Date(Number(response.list[7].dt) * 1000);
-            //console.log(d.toLocaleDateString());
-            rowForecastDate1.text(js_date.toLocaleDateString());
-            colForecastDay1.append(rowForecastDate1);
-
-            var colForecastDay2 = $("<div>").addClass("col-2");
-            var rowForecastDate2 = $("<p>").addClass("row");
-
-            var js_date = new Date(Number(response.list[15].dt) * 1000);
-            //console.log(d.toLocaleDateString());
-            rowForecastDate2.text(js_date.toLocaleDateString());
-            colForecastDay2.append(rowForecastDate2);
-
-
-            var colForecastDay3 = $("<div>").addClass("col-2");
-            var rowForecastDate3 = $("<p>").addClass("row");
-
-            var js_date = new Date(Number(response.list[23].dt) * 1000);
-            //console.log(d.toLocaleDateString());
-            rowForecastDate3.text(js_date.toLocaleDateString());
-            colForecastDay3.append(rowForecastDate3);
-
-
-            var colForecastDay4 = $("<div>").addClass("col-2");
-            var rowForecastDate4 = $("<p>").addClass("row");
-
-            var js_date = new Date(Number(response.list[31].dt) * 1000);
-            //console.log(d.toLocaleDateString());
-            rowForecastDate4.text(js_date.toLocaleDateString());
-            colForecastDay4.append(rowForecastDate4);
-
-            var colForecastDay5 = $("<div>").addClass("col-2");
-            var rowForecastDate5 = $("<p>").addClass("row");
-
-            var js_date = new Date(Number(response.list[39].dt) * 1000);
-            //console.log(d.toLocaleDateString());
-            rowForecastDate5.text(js_date.toLocaleDateString());
-            colForecastDay5.append(rowForecastDate5);
-
-
-            // var rowForecastIcon = $("<img>").addClass("row");
-            // rowForecastIcon.attr(
-            //        "src",
-            //        "http://openweathermap.org/img/w/" + response.list[7].weather[0].icon + ".png"
-            //  );
-            // colForecastDay1.append(rowForecastIcon);
-
-            // var rowForecastTemp   = $("<div>").addClass("row");
-            // rowForecastTemp.text("Temp: " + response.list[7].main.temp + " \u00B0F");
-            // colForecastDay1.append(rowForecastTemp);
-
-            // var rowForecastHumidity = $("<div>").addClass("row");
-            // rowForecastHumidity.text("Humidity: " + response.list[7].main.humidity + "%");
-            // colForecastDay1.append(rowForecastHumidity);
-
-            //for (let i = 7; i < 40; i = i + 8) {
-            //   var d = new Date(Number(response.list[i].dt) * 1000);
-            //   console.log(d.toLocaleString(), response.list[i].main.temp, response.list[i].main.humidity,
-            //   response.list[i].main.temp_min, response.list[i].main.temp_max);
-            //}
-
-            forecastRow.append(colForecastDay1, colForecastDay2, colForecastDay3, colForecastDay4, colForecastDay5);
+            //   var forecastRowTitle = $("<p>").addClass("h4");
+            //   forecastRowTitle.text("5-Day Forecast:")
+            //   forecastRow.append(forecastRowTitle);
 
             $currentWeather.append(forecastRow);
+
+            //------------------------------------
+            // loop will start here
+            //------------------------------------
+
+            // DAY 1
+
+            var forecastDay1 = $("<div>").addClass("forecast-box");
+
+            createForecastDay(forecastDay1, 7, response);
+
+            // DAY-2
+
+            var forecastDay2 = $("<div>").addClass("forecast-box");
+
+            createForecastDay(forecastDay2, 15, response);
+
+            // DAY-3
+
+            var forecastDay3 = $("<div>").addClass("forecast-box");
+
+            createForecastDay(forecastDay3, 23, response);
+
+            // DAY-4
+
+            var forecastDay4 = $("<div>").addClass("forecast-box");
+
+            createForecastDay(forecastDay4, 31, response);
+
+            // DAY-5
+
+            var forecastDay5 = $("<div>").addClass("forecast-box");
+
+            createForecastDay(forecastDay5, 39, response);
+
+
+            // for (let i = 0; i < 40; i++) {
+            //     //for (let i = 7; i < 40; i = i + 8) {
+            //     var d = new Date(Number(response.list[i].dt) * 1000);
+            //     console.log(d.toLocaleString(), response.list[i].main.temp, response.list[i].main.humidity,
+            //         response.list[i].main.temp_min, response.list[i].main.temp_max);
+            // }
+
+            $currentWeather.append(forecastDay1, forecastDay2, forecastDay3, forecastDay4, forecastDay5);
 
         });
 
     }
+
+    function createForecastDay(forecastDay, idx, resp) {
+
+        var forecastDate = $("<p>");
+
+        var js_date = new Date(Number(resp.list[idx].dt) * 1000);
+        //console.log(d.toLocaleDateString());
+        forecastDate.text(js_date.toLocaleDateString());
+        forecastDay.append(forecastDate);
+
+        var forecastIcon = $("<img>");
+        forecastIcon.attr(
+            "src",
+            "http://openweathermap.org/img/w/" + resp.list[idx].weather[0].icon + ".png"
+        );
+        forecastDay.append(forecastIcon);
+
+        var forecastTemp = $("<p>");
+        forecastTemp.text("Temp: " + resp.list[idx].main.temp + " \u00B0F");
+        forecastDay.append(forecastTemp);
+
+        var forecastHumidity = $("<p>");
+        forecastHumidity.text("Humidity: " + resp.list[idx].main.humidity + "%");
+        forecastDay.append(forecastHumidity);
+    }
+
 
     //-------------------------------------------------------------------------------------
     // function to get the UV Index for the city in question using latitude and longitude
@@ -258,32 +292,55 @@ $(document).ready(function () {
     //-------------------------------------------------------------------------------------
 
     function getUVIndex(cityId, lat, lon) {
-    
+
         // set up the AJAX query URL
-    
+
         console.log("Get UV Index for lat (" + lat + ") and lon (" + lon + ")");
         //return;
-        
-        var queryURL =     
+
+        var queryURL =
             "http://api.openweathermap.org/data/2.5/uvi?appid=" +
             APIKey
-            + 
+            +
             "&units=imperial&lat=" + lat + "&lon=" + lon;
 
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function(response) {
+        }).then(function (response) {
             console.log(response);
             //var now = moment().format("MMMM Do, YYYY");
             console.log(response.value);
 
             var uvIndexRow = $("<div>").addClass("row");
             var uvIndexSpan = $("<span>").attr("id", "uv-intensity");
+
+            var uvIndexNum = Number(response.value);
+            console.log("UV Index number: ", uvIndexNum);
+
+            // add class to UV Index to indicate whether value is low, medium, high or severe
+            if (uvIndexNum < 3) {
+                uvIndexSpan.addClass("favorable");
+                console.log("favorable");
+            }
+            else if (uvIndexNum < 6) {
+                uvIndexSpan.addClass("moderate");
+                console.log("moderate");
+            }
+            else if (uvIndexNum < 8) {
+                uvIndexSpan.addClass("high");
+                console.log("high");
+            }
+            else {
+                uvIndexSpan.addClass("severe");
+                console.log("severe");
+            }
+
             uvIndexSpan.text(" " + response.value);
             // uvIndexRow.text("UV Index: " + " <span>" + " " + response.value +"</span>");
             uvIndexRow.text("UV Index: ");
             uvIndexRow.append(uvIndexSpan);
+
 
             $currentWeather.append(uvIndexRow);
 
@@ -310,7 +367,7 @@ $(document).ready(function () {
     //<button type="button" class="list-group-item list-group-item-action">Phoenix</button>
     //const date = new Date(1582059600000).toDateString();
     //console.log(date);
- 
+
             //   Forecast: 
         //var queryURL = 
         // "http://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&units=imperial&appid=" +
