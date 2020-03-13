0b7ee1c314d07a4c78cef1cf125d72260f466eae
Jim Greasley <azjimgreasley@gmail.com>
2020-03-13 09:02:42 -0700

    improve formatting of current weather data

--- js/javascript.js
+++ js/javascript.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     // This is my API key.
     var APIKey = "7514abfe02ab6db7877685958ec119d7";
 
-    // get Weather Cities history data from local storage
+    // testing - delete Weather Cities history data from local storage
     //localStorage.removeItem("WeatherCities");
 
     var data = localStorage.getItem("WeatherCities");
@@ -71,10 +71,11 @@ $(document).ready(function () {
     //-----------------------------------------------------------------------------------------
 
     function saveSearchCity(cityName, cityID) {
-
-        // search weather_cities array for the current search city name
-        // if not found then add it to the array
-        // save weather_cities array in local storage
+        // ---------------------------------------------------------------
+        // Search weather_cities array for the current search city name.
+        // If not found then add it to the array and save weather_cities
+        // array in local storage.
+        //---------------------------------------------------------------
         console.log("Save Srch City");
         var cityIndex = -1;
         weather_cities.forEach(checkForCity);
@@ -105,7 +106,9 @@ $(document).ready(function () {
         }
     }
 
+    //--------------------------------------------------------------------
     // Constructor function for City objects in Weather Cities array
+    //--------------------------------------------------------------------
 
     function City(cityName, cityID) {
         this.name = cityName;
@@ -148,13 +151,17 @@ $(document).ready(function () {
 
 
 
+    //-------------------------------------------------------------------------------------
+    // Function to get the current weather conditions either by city name or city ID if 
+    // selected from city history list.
+    //-------------------------------------------------------------------------------------
 
     function getCurrentWeather(cityName, cityId) {
 
         // clear current weather area before displaying any new data
         $currentWeather.empty();
 
-        // set up the AJAX query URL
+        // set up the AJAX query URL using city ID (if one) or city name
 
         if (cityId === null) {
             console.log("Search for city: ", cityName);
@@ -188,6 +195,7 @@ $(document).ready(function () {
             var longitude = response.coord.lon;
             var cityID = response.id;
 
+            // build city row using city name, current date and current weather icon from ajax response
             var cityRow = $("<div>").addClass("row");
 
             var colCityDate = response.name + ' (' + moment().format("l") + ')';
@@ -205,21 +213,43 @@ $(document).ready(function () {
 
             $currentWeather.append(cityRow);
 
+            // build temperature row using current temperature from ajax response
             var tempRow = $("<div>").addClass("row");
-            tempRow.text("Temperature: " + response.main.temp + " \u00B0F");
+
+            var colTempLit = $("<div>").addClass("col-2");
+            colTempLit.text("Temperature:");
+            var colTempVal = $("<div>").addClass("col-2");
+            colTempVal.text(response.main.temp + " \u00B0F");
+            tempRow.append(colTempLit, colTempVal);
+
             $currentWeather.append(tempRow);
 
+            // build humidity row using current humidity value from ajax response
             var humidityRow = $("<div>").addClass("row");
-            humidityRow.text("Humidity: " + response.main.humidity + "%");
+
+            var colHumLit = $("<div>").addClass("col-2");
+            colHumLit.text("Humidity:");
+            var colHumVal = $("<div>").addClass("col-2");
+            colHumVal.text(response.main.humidity + "%");
+            humidityRow.append(colHumLit, colHumVal);
+
             $currentWeather.append(humidityRow);
 
+            // build wind speed row using current wind speed value from ajax response
             var windSpeedRow = $("<div>").addClass("row");
-            windSpeedRow.text("Wind Speed: " + response.wind.speed + "MPH");
+
+            var colWindLit = $("<div>").addClass("col-2");
+            colWindLit.text("Wind Speed:");
+            var colWindVal = $("<div>").addClass("col-2");
+            colWindVal.text(response.wind.speed + "MPH");
+            windSpeedRow.append(colWindLit, colWindVal);
+
             $currentWeather.append(windSpeedRow);
 
+            // need to use longitude & latitude (from ajax response above) to get UV Index value
             getUVIndex(cityID, latitude, longitude);
 
-            // expand this to save city name and cityID in array of objects
+            // city name and cityID from response in city history array (of objects)
             saveSearchCity(cityName, cityID);
 
         })
@@ -251,12 +281,14 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
+            //console.log(response);
             //var now = moment().format("MMMM Do, YYYY");
-            console.log(response.value);
+            //console.log(response.value);
 
             var uvIndexRow = $("<div>").addClass("row");
+
             var uvIndexSpan = $("<span>").attr("id", "uv-intensity");
+            uvIndexSpan.text(response.value);
 
             var uvIndexNum = Number(response.value);
             console.log("UV Index number: ", uvIndexNum);
@@ -264,26 +296,28 @@ $(document).ready(function () {
             // add class to UV Index to indicate whether value is low, medium, high or severe
             if (uvIndexNum < 3) {
                 uvIndexSpan.addClass("favorable");
-                console.log("favorable");
+                //console.log("favorable");
             }
             else if (uvIndexNum < 6) {
                 uvIndexSpan.addClass("moderate");
-                console.log("moderate");
+                //console.log("moderate");
             }
             else if (uvIndexNum < 8) {
                 uvIndexSpan.addClass("high");
-                console.log("high");
+                //console.log("high");
             }
             else {
                 uvIndexSpan.addClass("severe");
-                console.log("severe");
+                //console.log("severe");
             }
 
-            uvIndexSpan.text(" " + response.value);
-            // uvIndexRow.text("UV Index: " + " <span>" + " " + response.value +"</span>");
-            uvIndexRow.text("UV Index: ");
-            uvIndexRow.append(uvIndexSpan);
+            var colUvIndexLit = $("<div>").addClass("col-2");
+            colUvIndexLit.text("UV Index:");
 
+            var colUvIndexVal = $("<div>").addClass("col-1");
+            colUvIndexVal.append(uvIndexSpan);
+            
+            uvIndexRow.append(colUvIndexLit, colUvIndexVal);
 
             $currentWeather.append(uvIndexRow);
 
@@ -346,19 +380,19 @@ $(document).ready(function () {
                 var js_d = new Date(Number(response.list[idx].dt) * 1000);
                 var forecastDateTime = js_d.toLocaleString();
                 //console.log("forecast DateTime ", forecastDateTime);
-                // isolate date portion (mm/dd/yyyy) from begining of date string
+                // isolate date portion (mm/dd/yyyy) from begining of forecast 'date+time' string
                 var pos_comma = forecastDateTime.indexOf(",");
                 var compareDate = forecastDateTime.slice(0, pos_comma);
                 //console.log("compare date: ", compareDate, " search date: ", searchDate);
                 if (compareDate !== searchDate) {
-                    // the forecast data has changed to a new day, so replace the
+                    // The forecast data has changed to a new day, so replace the
                     // search date with the new forecast date and trigger search based on time
                     var saveForecastDate = compareDate;
                     searchDate = compareDate;
                     searchForTime = true;
                 } else {
                     if (searchForTime) {
-                        // isolate the time portion of the
+                        // isolate the time portion from end of forecast 'date+time' string
                         var searchTime = forecastDateTime.slice(pos_comma + 2);
                         if (searchTime === "2:00:00 PM") {
                             var newForecast = new Forecast(
@@ -367,14 +401,16 @@ $(document).ready(function () {
                                 response.list[idx].main.temp,
                                 response.list[idx].main.humidity
                             );
-                            fiveDayForecast.push(newForecast); 
+                            fiveDayForecast.push(newForecast);
+                            // found the time slot we want so stop searching for time match
+                            searchForTime = false;
                         }
                     }
                 }
                 idx++
             } while (idx < response.cnt);
 
-            // need to save last entry in response array as the fifth entry in 5-day forecast
+            // Need to save last entry in response array as the fifth entry in 5-day forecast
             // if inquiring before 2 pm.
             if (fiveDayForecast.length < 5) {
                 var newForecast = new Forecast(
@@ -387,9 +423,19 @@ $(document).ready(function () {
             }
 
             console.log(fiveDayForecast);
-        
-            var forecastRow = $("<div>").addClass("row h4");
-            forecastRow.text("5-Day Forecast:");
+
+            // add blank line before forecast area
+            var forecastRow = $("<br>");
+
+            $currentWeather.append(forecastRow);
+
+            // create forecast block title 
+            var forecastRow = $("<div>").addClass("row");
+
+            var colForecastTitleLit = $("<div>").addClass("col-3 h4");
+            colForecastTitleLit.text("5-Day Forecast:");
+
+            forecastRow.append(colForecastTitleLit);
 
             $currentWeather.append(forecastRow);
 
@@ -430,8 +476,9 @@ $(document).ready(function () {
         });
     }
 
-
+    //------------------------------------------------------
     // Constructor for 5-day forecast objects
+    //------------------------------------------------------
     function Forecast(fcDate, fcIcon, fcTemp, fcHumidity) {
         this.forecastDate     = fcDate;
         this.forecastIcon     = fcIcon;
@@ -439,7 +486,10 @@ $(document).ready(function () {
         this.forecastHumidity = fcHumidity;
     }
 
-
+    //------------------------------------------------------
+    // Function to create a forecast Day box consisting of
+    //  - date, icon and temperature and humidity values.
+    //------------------------------------------------------
     function createForecastDay(forecastDay, forecast) {
 
         var forecastDate = $("<p>");
@@ -463,40 +513,4 @@ $(document).ready(function () {
         forecastDay.append(forecastHumidity);
     }
 
-    // var js_date = new Date(Number(resp.list[idx].dt) * 1000);
-    // //console.log(d.toLocaleDateString());
-    // forecastDate.text(js_date.toLocaleDateString());
-    // forecastDay.append(forecastDate);
-
-    // var forecastIcon = $("<img>");
-    // forecastIcon.attr(
-    //     "src",
-    //     "https://openweathermap.org/img/w/" + resp.list[idx].weather[0].icon + ".png"
-    // );
-    // forecastDay.append(forecastIcon);
-
-    // var forecastTemp = $("<p>");
-    // forecastTemp.text("Temp: " + resp.list[idx].main.temp + " \u00B0F");
-    // forecastDay.append(forecastTemp);
-
-    // var forecastHumidity = $("<p>");
-    // forecastHumidity.text("Humidity: " + resp.list[idx].main.humidity + "%");
-    // forecastDay.append(forecastHumidity);
- 
 });
-
-    //var d = new Date();
-    //console.log(typeof d);
-    //var n = d.toLocaleDateString();
-    //console.log(d.toLocaleDateString());
-
-    // will be for clear button
-    //$("#clear-link").on("click", function(event) {
-    //    event.preventDefault();
-    //    $("#result-card").empty();
-    //});
-
-    //<div class="list-group" id="city-history">
-    //<button type="button" class="list-group-item list-group-item-action">Phoenix</button>
-    //const date = new Date(1582059600000).toDateString();
-    //console.log(date);
